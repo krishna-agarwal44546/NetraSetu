dde756b51e8b94d03492a17a30186c8e476e2f18
krishna-agarwal44546 <krishnaiit7@gmail.com>
2026-09-20 02:00:19 +0530

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://netrasetu-2.onrender.com";
 
 function App() {
   const [file, setFile] = useState(null);
